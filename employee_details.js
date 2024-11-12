b6593d3f45184e314202514293ae93f4bb0b3fa3
Sebastian999999 <i191994@nu.edu.pk>
2024-11-12 08:49:25 -0500

    Practiced array methods

---
+++ employee_details.js
@@ -0,0 +1,30 @@
+const employees = [
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      //... More employee records can be added here
+    ];
+
+    function displayEmployees(){
+        const totalEmployees = employees.map((employee , index) => `${employee.id} ${employee.name} ${employee.age} ${employee.department} ${employee.salary}`).join('');
+        document.getElementById('employeesDetails').innerHTML = totalEmployees;
+    }
+
+    function calculateTotalSalaries(){
+        const totalsalary = employees.reduce((total , emp) => total + emp.salary , 0);
+        document.getElementById('employeesDetails').innerHTML = `<p>Total Salary : ${totalsalary}</p>`;
+    }
+
+    function displayHREmployees(){
+        const HREmployees = employees.filter((emp)=>emp.department==='HR');
+        const HREmployee = HREmployees.map((emp,index)=>`<p>${emp.id} ${emp.name} ${emp.age} ${emp.department} ${emp.salary}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML=HREmployee;
+    }
+
+    function findEmployeeById(id){
+        const employeeID = employees.find(emp => emp.id === id);
+        if (employeeID)
+            document.getElementById('employeesDetails').innerHTML = `<p>${employeeID.id} ${employeeID.name} ${employeeID.age} ${employeeID.department} ${employeeID.salary}</p>`;
+        else
+            document.getElementById('employeesDetails').innerHTML = '<p>Employee not found.</p>';
+    }
